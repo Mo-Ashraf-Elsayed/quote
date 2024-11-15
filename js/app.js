cfc0930dd91e154da3dd810e0d59a0dabdd1ecca
M-Ashraf-Elsayed <pop616631@gmail.com>
2024-11-15 16:54:49 +0200

    first commit

---
+++ js/app.js
@@ -0,0 +1,24 @@
+var data = [
+  `“Be yourself; everyone else is already taken.” <br>― Oscar Wilde`,
+  `“So many books, so little time.”
+<br> ― Frank Zappa`,
+  `“A room without books is like a body without a soul.”
+<br> ― Marcus Tullius Cicero`,
+  `“You only live once, but if you do it right, once is enough.”
+<br> ― Mae West`,
+  `“Be the change that you wish to see in the world.”
+<br> ― Mahatma Gandhi`,
+  `“If you tell the truth, you don't have to remember anything.”
+<br> ― Mark Twain`,
+];
+var ele = document.getElementById("quote");
+var lastNum;
+function showData() {
+  var num = Math.floor(Math.random() * data.length);
+  while (lastNum === num) {
+    num = Math.floor(Math.random() * data.length);
+  }
+  ele.innerHTML = data[num];
+  lastNum = num;
+  //   console.log(lastNum);
+}
